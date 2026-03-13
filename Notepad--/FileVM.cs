1124a4eb091a8c9a37aacbf3144de4fda8a8c9b0
Bulgaru Bogdan <bulgarubogdan11@gmail.com>
2026-03-13 11:42:59 +0200

    Implemented default functionalities

---
+++ Notepad--/FileVM.cs
@@ -0,0 +1,108 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+using Microsoft.Win32;
+
+    namespace Notepad__
+    {
+        public class FileVM : INotifyPropertyChanged
+        {
+        private string name;
+        private string path;
+
+        private string content;
+
+        private bool isDirectory;
+            public string Name
+            {
+                get => name;
+                set { name = value; OnPropertyChanged(nameof(Name)); }
+            }
+
+            public string Path
+            {
+                get => path;
+                set { path = value; OnPropertyChanged(nameof(Path)); }
+            }
+
+            public string Content
+            {
+                get => content;
+                set { content = value; OnPropertyChanged(nameof(Content)); }
+            }
+
+            public bool IsDirectory
+            {
+                get => isDirectory;
+                set { isDirectory = value; OnPropertyChanged(nameof(IsDirectory)); }
+            }
+        public ICommand NewFileCommand { get; }
+        public ICommand OpenFileCommand { get; }
+        public ICommand SaveFileCommand { get; }
+        public FileVM()
+        {
+            NewFileCommand = new RelayCommand(_ => NewFile());
+            OpenFileCommand = new RelayCommand(_ => OpenFile());
+            SaveFileCommand = new RelayCommand(_ => SaveFile(), _ => !string.IsNullOrEmpty(Content));
+        }
+        public FileVM(string name, string path, bool isDirectory)
+        {
+            Name = name;
+            Path = path;
+            IsDirectory = isDirectory;
+
+            NewFileCommand = new RelayCommand(_ => NewFile());
+            OpenFileCommand = new RelayCommand(_ => OpenFile());
+            SaveFileCommand = new RelayCommand(_ => SaveFile(), _ => !string.IsNullOrEmpty(Content));
+
+            if (!isDirectory && File.Exists(path))
+                Content = File.ReadAllText(path);
+        }
+        private void NewFile()
+        {
+            Name = "";
+            Path = "";
+            Content = "";
+        }
+
+        private void OpenFile()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Fisiere text (*.txt)|*.txt|Toate (*.*)|*.*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                Path = dialog.FileName;
+                Name = System.IO.Path.GetFileName(Path);
+                Content = File.ReadAllText(Path);
+            }
+        }
+
+        private void SaveFile()
+        {
+            if (string.IsNullOrEmpty(Path))
+            {
+                var dialog = new SaveFileDialog
+                {
+                    Filter = "Fisiere text (*.txt)|*.txt"
+                };
+
+                if (dialog.ShowDialog() == true)
+                    Path = dialog.FileName;
+                else
+                    return;
+            }
+
+            File.WriteAllText(Path, Content);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string nume) =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nume));
+        }
+    }
---
+++ Notepad--/MainVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad__
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<FileVM> FileDeschise { get; } = new ObservableCollection<FileVM>();
+
+        private FileVM _fileActiv;
+        public FileVM FileActiv
+        {
+            get => _fileActiv;
+            set { _fileActiv = value; OnPropertyChanged(nameof(FileActiv)); }
+        }
+
+        public MainVM()
+        {
+            var fisierInitial = new FileVM();
+            FileDeschise.Add(fisierInitial);
+            FileActiv = fisierInitial;
+        }
+    }
+}
